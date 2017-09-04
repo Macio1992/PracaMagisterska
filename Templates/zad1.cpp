da564569a51dc63bb8e1ee171b4e972a96ce8142
Macio1992 <maix19bep@gmail.com>
2017-09-04 21:45:03 +0200

    new

--- Templates/zad1.cpp
+++ Templates/zad1.cpp
@@ -1,90 +1,16 @@
 #include <iostream>
+// #include <origin-master/origin/core/concepts.hpp>
+#include "origin-master/origin/core/concepts.hpp"
 
 using namespace std;
-/*
-    template<int i>
-    class LOOP{
-        public:
-            static inline void EXEC(){
-                cout <<"A-"<<i<<" ";
-                LOOP<i-1>::EXEC();
-                cout<<"B-"<<i<<" ";
-            }
-    };
+using namespace origin;
 
-    template<>
-    class LOOP<0>{
-        public:
-            static inline void EXEC(){
-                cout<<"A-"<<0;
-                cout<<"\n";
-                cout<<"B-"<<0;
-            }
-    };
-
-    template<bool Condition>
-    class IF {
-        public:
-            static inline void EXEC(){
-                cout<<"Statement is true";
-            }
-    };
-
-    template<>
-    class IF<false> {
-        public:
-            static inline void EXEC(){
-                cout<<"Statement is false";
-            }
-    };
-
-    template<int _case>
-    class SWITCH {
-        public:
-            static inline void EXEC(){
-                cout<<"SWITCH-default ";
-            }
-    };
-
-    template<>
-    class SWITCH<1> {
-        public:
-            static inline void EXEC(){
-                cout<<"SWITCH - 1 ";
-            }
-    };
-
-    template<>
-    class SWITCH<2> {
-        public:
-            static inline void EXEC(){
-                cout<<"SWITCH - 2 ";
-            }
-    };
-*/
-
-void print(int i){
-    cout<<i<<endl;
-}
-
-void print(const string &i){
-    cout<<i<<endl;
-}
-
-void print(double i){
-    cout<<i<<endl;
-}
-
-void print(bool i){
-    cout<<i<<endl;
-}
+template<typename T, typename S>
+requires Equality_comparable<T>()
+void find(T t){}
 
 int main () {
-    
-    /*SWITCH<2> x;
-    x.EXEC();
-    IF<false>::EXEC();
-    x.EXEC();*/
+
 
     return 0;
 }
