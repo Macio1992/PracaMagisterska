78549e4ba2ba23ec62e11fe57f3a18126bae29c5
Macio1992 <maix19bep@gmail.com>
2017-09-06 03:12:36 +0200

    new

--- Templates/zad1.cpp
+++ Templates/zad1.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
+#include <iterator>
+#include <vector>
 // #include <origin-master/origin/core/concepts.hpp>
-#include "origin-master/origin/core/concepts.hpp"
+// #include "origin-master/origin/core/concepts.hpp"
 
 using namespace std;
-using namespace origin;
-
-template<typename T, typename S>
-requires Equality_comparable<T>()
-void find(T t){}
+// using namespace origin;/
 
 int main () {
 
+    vector<int> v {3,1,4};
+    auto v1 = v.begin();
+
+    advance(v1, 2);
+
+    cout<<*v1<<endl;
 
     return 0;
 }
